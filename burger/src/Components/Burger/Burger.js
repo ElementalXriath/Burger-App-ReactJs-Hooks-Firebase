ebd52b8d94bfbb9d0c29aede37a7e90cac643352
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2019-12-14 16:20:48 -0500

    Created Hoc , added ingredients, next is add them dynamically

---
+++ burger/src/Components/Burger/Burger.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Burger.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
+
+const burger = (props) => {
+
+    const transformIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+          return  <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    return (
+        <div className="Burger">
+            <BurgerIngredient type="bread-top" />
+            {transformIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
---
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../Components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+    state ={
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <Aux>
+
+                <div>
+                    <Burger ingredients={this.state.ingredients}/>
+                </div>
+                <div>
+                    Build Controller
+                </div>
+
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
